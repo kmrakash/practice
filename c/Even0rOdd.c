2c20e334a08dc4a6137b96e35091b1be286e052d
Akash Kumar <aksh.iem22@gmail.com>
2019-05-18 23:18:54 +0530

    Using Bitwise Operator

--- c/Even0rOdd.c
+++ c/Even0rOdd.c
@@ -4,7 +4,7 @@ void main()
     int num;
     scanf("%d",&num);
     printf("%d\n",num>>1);
-    printf("%d\n",(num>>1)<<1);
+    printf("%d\n",(num>>1)<<1); /* shifting left first loses 1 from bitwise */
     if((num>>1)<<1==num)
     printf("Even");
     else
