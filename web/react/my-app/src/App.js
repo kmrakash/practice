e933ea05ee9e5240794f1739736c44b65ac40682
Akash Kumar <aksh.iem22@gmail.com>
2020-04-23 21:01:27 +0530

    Basics

---
+++ web/react/my-app/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <h1>Hi , I am a react App</h1>
+      </div>
+    );
+
+
+    // All code above is compiled to code like below
+    // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'This is inside'));
+  }
+}
+
+export default App;
