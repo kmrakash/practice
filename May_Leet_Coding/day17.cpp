99206de683566365558456f99c9ba2b63196c412
Akash Kumar <aksh.iem22@gmail.com>
2020-05-18 17:08:06 +0530

    Anagrams

---
+++ May_Leet_Coding/day17.cpp
@@ -0,0 +1,53 @@
+/*	Find starting index of All Anagrams.
+ * 
+ * */
+
+
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int ind[100];
+
+int findSum(string x){
+		int ans = 0;
+		for(int i=0; i<x.length(); i++){
+				ans += (int)x[i];
+		}
+		
+		return ans;
+}
+
+void finAnagrams(string s, string p){
+	int len_p = p.length();
+	int len_s = s.length();
+	
+	int sumP = findSum(p);
+	cout << "P sum: " << sumP << "\n";
+	int j=0;
+	for(int i=0; i<=len_s-len_p ; i++){
+			string sub = s.substr(i, len_p);
+			int sumS = findSum(sub);
+			cout << "Substring is " << sub << "\n" << "It's sum is " << sumS << "\n";
+			if(sumS==sumP) {
+				ind[j++] = i;
+			}
+	}
+	cout << j << "\n";
+	for(int i=0; i<j; i++){
+			cout << ind[i] << " ";
+	}
+	
+}
+
+int main(){
+	string s , p;
+	getline(cin, s);
+	getline(cin , p);
+	
+	finAnagrams(s, p);
+	
+	//~ cout << "You Enter " << s << " " << p << "\n";
+		return 0;
+}
---
+++ May_Leet_Coding/day18.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<string>
+#include<map>
+#include<utility>
+#include<algorithm>
+using namespace std;
+
+bool checkInclusion(string s1, string s2) {
+        int map[26] = {0};
+        for(char c : s1) map[c - 'a']++;
+        int j = 0, i = 0, count_chars = s1.size();
+        while(j < s2.size()){
+            if(map[s2.at(j++) - 'a']-- > 0)
+                count_chars--;
+            if(count_chars == 0) return true;
+            if(j - i == s1.size() && map[s2.at(i++) - 'a']++ >= 0)
+                count_chars++;
+        }
+        return false;
+}
+
+
+int main(){
+	string s , p;
+	getline(cin, s);
+	getline(cin , p);
+	
+	cout << checkInclusion(s, p) << "\n";
+		return 0;
+}
+
+/*class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s2.length() < s1.length()) {
+            return false;
+        }
+        if (s1 == "") {
+            return true;
+        }
+
+        vector<int> v(26, 0);
+        for (char c : s1) {
+            v[c - 'a']++;
+        }
+
+        vector<int> s(26, 0);
+        for (int i = 0; i < s1.length() - 1; i++) {
+            s[s2[i] - 'a']++;
+        }
+
+        for (int i = s1.length() - 1; i < s2.length(); i++) {
+            s[s2[i] - 'a']++;
+            if (compare(s, v)) {
+                return true;
+            }
+            s[s2[i - (s1.length() - 1)] - 'a']--;
+        }
+
+        return false;
+    }
+
+private:
+    bool compare(vector<int> &v1, vector<int> &v2) {
+        for (int i = 0; i < v1.size(); i++) {
+            if (v1[i] != v2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};*/
