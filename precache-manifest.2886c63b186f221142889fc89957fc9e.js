f2c6bd04521790f166bf45f48f59be1bb08d98c2
Akash Kumar <aksh.iem22@gmail.com>
2020-06-03 15:33:56 +0530

    Updates

---
+++ precache-manifest.2886c63b186f221142889fc89957fc9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fbf7495c1abf0ebe4dc9d8f15f62793",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "493e839bfd66dfb98aaa",
+    "url": "/monster-rolodex/static/css/main.350392c7.chunk.css"
+  },
+  {
+    "revision": "ce7dfa65aa771e92a827",
+    "url": "/monster-rolodex/static/js/2.066476fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.066476fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "493e839bfd66dfb98aaa",
+    "url": "/monster-rolodex/static/js/main.e0ba1c7f.chunk.js"
+  },
+  {
+    "revision": "753b30f80137f55e537c",
+    "url": "/monster-rolodex/static/js/runtime-main.e7a3ea5b.js"
+  }
+]);
